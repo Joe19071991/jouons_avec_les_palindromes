30fe9d92c2dde1778e4ef82c9102d74170357367
Joe19071991 <lauranove66@gmail.com>
2024-11-22 10:25:05 +0100

    format date

---
+++ script.js
@@ -0,0 +1,9 @@
+function isValidDate(date) {
+  // Je vérifie que la date a bien le format "dd/mm/yyyy"
+  const regex = /^\d{2}\/\d{2}\/\d{4}$/; // expression régulière trouvé sur le internet
+  if (regex.test(date) === false) {
+    return false; // Si le format est incorrect, retourner false
+  }
+}
+console.log(isValidDate(13568888));
+console.log(isValidDate("12/11/2024")); // undefined
