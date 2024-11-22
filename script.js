8087fce94fb36a17894879f5dac0efa58ccb21f1
Joe19071991 <lauranove66@gmail.com>
2024-11-22 15:09:39 +0100

    étape-1,format + bissextiles

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
-function isValidDate(date) {
-  // Je vérifie que la date a bien le format "dd/mm/yyyy"
-  const regex = /^\d{2}\/\d{2}\/\d{4}$/; // expression régulière trouvé sur le internet
-  if (regex.test(date) === false) {
-    return false; // Si le format est incorrect, retourner false
-  }
-  //return true;
-  const partsOfDates = date.split("/");
-  const day = parseInt(partsOfDates[0], 10);
-  const month = parseInt(partsOfDates[1], 10);
-  const year = parseInt(partsOfDates[2], 10);
-
-  if (month < 1 || month > 12) {
-    return false;
-  }
-  if (year < 1000 || year > 9999) {
-    return false;
-  }
-  //return true;
-}
-
 function maxDaysInMonth(month, year) {
-  if (month === 2) {
-    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-      return 29;
-    } else {
-      return 28;
-    }
+  //les mois de janvier (1) à décembre (12)
+  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  //années bissextiles
+  if (month === 2 && year % 4 === 0 && (year !== 0 || year % 400 === 0)) {
+    return 29;
   }
-  if (month === 4 || month === 6 || month === 9 || month === 11) {
-    return 30;
-  }
-  return 31;
+
+  return daysInMonth[month - 1];
 }
 
-function isDateValid(day, month, year) {
-  const maxDays = maxDaysInMonth(month, year);
-  if (day > 0 && day <= maxDays) {
-    return true;
-  } else {
+function isValidDate(dateString) {
+  // Vérification du format (dd/mm/yyyy)
+  const dateParts = dateString.split("/");
+  if (dateParts.length !== 3) {
     return false;
   }
+
+  const day = parseInt(dateParts[0], 10);
+  const month = parseInt(dateParts[1], 10);
+  const year = parseInt(dateParts[2], 10);
+
+  if (isNaN(day) || isNaN(month) || isNaN(year)) {
+    return false;
+  }
+
+  if (month < 1 || month > 12 || year < 1000 || year > 9999) {
+    return false;
+  }
+  const maxDays = maxDaysInMonth(month, year);
+  if (day < 1 || day > maxDays) {
+    return false;
+  }
+
+  return true;
 }
 
-//format date :
-//console.log(isValidDate(13568888));
-//console.log(isValidDate("12/11/2024"));
-
-//vérification des mois/années
-//console.log(isValidDate("05/14/2024"));
-//console.log(isValidDate("18/02/1987"));
-
-//vérif nbr jour dans le mois est valide
-//console.log(isDateValid(29, 2, 2024));
-//console.log(isDateValid(31, 11, 2023));
-//console.log(isDateValid(18, 2, 1987));
+// vérification du format
+console.log(isValidDate("01 - 02 - 87")); // false
+//vérification si la date est true
+console.log(isValidDate("03/04/2001")); // true
+//vérification si date est false :
+console.log(isValidDate("03/14/2001")); // false
+//vérification année bissextile true:
+console.log(isValidDate("29/02/2020")); //true
